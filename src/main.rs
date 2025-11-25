10fbd910fc2fba842b5db7e09b9232104c6e60a9
hamza <khanhamzah142@gmail.com>
2025-11-25 23:04:24 +0530

    sending rays into the scene

--- src/main.rs
+++ src/main.rs
@@ -3,27 +3,55 @@ mod ray;
 mod vec3;
 
 use color::{write_color, Color};
+use ray::Ray;
+use vec3::{Point3, Vec3};
+
+pub fn ray_color(r: &Ray) -> Color {
+    Color::new(0., 0., 0.)
+}
 
 fn main() {
-    const IMAGE_WIDTH: i32 = 256;
-    const IMAGE_HEIGHT: i32 = 256;
+    //Image
+    const ASPECT_RATIO: f64 = 16. / 9.;
+    const IMAGE_WIDTH: i32 = 400;
+
+    //Calculate the image height and ensure that it's atleast 1.
+    //TODO: Ensure that it is atleast 1.
+
+    const IMAGE_HEIGHT: i32 = (IMAGE_WIDTH as f64 / ASPECT_RATIO) as i32;
+
+    // Camera
+    let focal_length = 1.;
+    let viewport_height = 2.;
+    let viewport_width = viewport_height * (IMAGE_WIDTH / IMAGE_HEIGHT) as f64;
+    let camera_center = Point3::new(0., 0., 0.);
+
+    // Calculate the vector across the horizontal and down the vertical viewport edtes.
+    let viewport_u = Vec3::new(viewport_width, 0., 0.);
+    let viewport_v = Vec3::new(0., viewport_height, 0.);
+
+    // Calculate the vertical and horizontal delta i.e. distance from pixel to pixel.
+    let pixel_delta_u = viewport_u / IMAGE_WIDTH as f64;
+    let pixel_delta_v = viewport_v / IMAGE_HEIGHT as f64;
+
+    //Calcualte the location of upper left pixel
+    let viewport_upper_left =
+        camera_center - Vec3::new(0., 0., focal_length) - viewport_u / 2. - viewport_v / 2.;
+    let pixel00_loc = viewport_upper_left + 0.5 * (pixel_delta_u + pixel_delta_v);
+
+    // Render
 
     print!("P3\n{} {}\n255\n", IMAGE_WIDTH, IMAGE_HEIGHT);
 
     for j in (0..IMAGE_HEIGHT).rev() {
         eprint!("\rScanlines remaining: {} ", j);
         for i in 0..IMAGE_WIDTH {
-            let r = i as f64 / (IMAGE_WIDTH - 1) as f64;
-            let g = j as f64 / (IMAGE_HEIGHT - 1) as f64;
-            let b = 1.;
+            let pixel_center =
+                pixel00_loc + (i as f64 * pixel_delta_u) + (j as f64 * pixel_delta_v);
+            let ray_direction = pixel_center - camera_center;
+            let r = Ray::new(camera_center, ray_direction);
 
-            // let ir = (255.999 * r) as i32;
-            // let ig = (255.999 * g) as i32;
-            // let ib = (255.999 * b) as i32;
-
-            // println!("{} {} {}\n", ir, ig, ib);
-
-            let pixel_color = Color::new(r, g, b);
+            let pixel_color = ray_color(&r);
             write_color(&mut std::io::stdout(), pixel_color);
         }
     }
