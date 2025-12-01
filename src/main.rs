73057c279cbe8e70ffae7539db8cf97cd153c064
hamza <khanhamzah142@gmail.com>
2025-12-01 22:18:53 +0530

    sphere generation

--- src/main.rs
+++ src/main.rs
@@ -4,9 +4,23 @@ mod vec3;
 
 use color::{write_color, Color};
 use ray::Ray;
-use vec3::{Point3, Vec3};
+use vec3::{dot, Point3, Vec3};
+
+pub fn hit_sphere(center: Point3, radius: f64, r: &Ray) -> bool {
+    let oc = center - r.origin();
+    let a = dot(r.direction(), r.direction());
+    let b = -2. * dot(r.direction(), oc);
+    let c = dot(oc, oc) - radius * radius;
+
+    let discriminant = b * b - 4. * a * c;
+
+    discriminant >= 0.0
+}
 
 pub fn ray_color(r: &Ray) -> Color {
+    if hit_sphere(Point3::new(0., 0., -1.), 0.5, r) {
+        return Color::new(1., 0., 0.);
+    }
     let unit_direction: Vec3 = vec3::unit_vector(r.direction());
     let a = 0.5 * (unit_direction.y() + 1.);
     (1. - a) * Color::new(1., 1., 1.) + a * Color::new(0.5, 0.7, 1.0)
